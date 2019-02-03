3f2d7573ced1066ccc404239e66fed81dc3c9c7c
joaquinlpereyra <joaquinlpereyra@gmail.com>
2019-02-03 00:26:31 -0300

    test and better at same time

--- main.go
+++ main.go
@@ -12,31 +12,35 @@ func pastMonthCommits() (int, error) {
 	wc := exec.Command("wc", "-l")
 	wcPipe, err := wc.StdinPipe()
 	if err != nil {
+		fmt.Println("error creating pipe")
 		return 0, err
 	}
-	commits, err := exec.Command("git", "log", "--since=\"last month\"", "--prety=format:'%s'").Output()
+	commits, err := exec.Command("git", "log", "--since='last month'", "pretty=format:'%s'").Output()
 	if err != nil {
+		fmt.Println("error executing git log")
 		return 0, err
 	}
-	wcPipe.Write(commits)
+	go func() {
+		defer wcPipe.Close()
+		wcPipe.Write(commits)
+
+	}()
 	amount, err := wc.CombinedOutput()
 	if err != nil {
+		fmt.Println("error getting output")
 		return 0, err
 	}
 	return int(binary.BigEndian.Uint16(amount)), nil
 }
 
 func main() {
-	salary := os.Getenv("salary")
-	if salary == "" {
-		salary = os.Getenv("notmysalary")
-	}
-	if salary == "" {
-		salary = os.Getenv("chanchite")
-	}
+	salary := os.Getenv("SALARY")
+	fmt.Println("HERE")
+	fmt.Println(salary)
 	intSalary, err := strconv.Atoi(salary)
 	if err != nil {
-		fmt.Printf("Check your env variable is a valid int")
+		println("WOOLOL")
+		fmt.Printf("Check your env variable is a valid int: %s", salary)
 		os.Exit(1)
 	}
 	commits, err := pastMonthCommits()
